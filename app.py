e274c34fabee868b8b60c97ba17caeb803ef2db2
Mitul Desai <34889290+Mitul2991@users.noreply.github.com>
2023-03-21 03:43:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-import pytorch_model as ptm
+import onnx
+import onnxruntime
 import torch
+import numpy as np
+from torchvision import transforms
 
 def init():
     global model
-    model = ptm.Classifier(ptm.BasicBlock, [2, 2, 2, 2])
-    model.load_state_dict(torch.load("pytorch_model_weights.pth"))
-    model.eval()
+    model = onnxruntime.InferenceSession('model.onnx')
     return model
+
+def preprocess_numpy(img):
+        resize = transforms.Resize((224, 224))   #must same as here
+        crop = transforms.CenterCrop((224, 224))
+        to_tensor = transforms.ToTensor()
+        normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        img = resize(img)
+        img = crop(img)
+        img = to_tensor(img)
+        img = normalize(img)
+        return img
 
 def inference(model_inputs:dict) -> dict:
     model = init()
     img = model_inputs.get('prompt')
     if img == None:
         return {'message' : 'No image provided'}
-    inp = model.preprocess_numpy(img).unsqueeze(0)
-    res = model.forward(inp)
-    output = {'class_id' : torch.argmax(res)}
+    img = preprocess_numpy(img)
+    input_name = model.get_inputs()[0].name
+    img = np.array(np.expand_dims(img, axis=0))
+    pred_class_id = model.run(None, {input_name : img})[0]
+    output = {'predicted_class_id' : pred_class_id}
     return output
