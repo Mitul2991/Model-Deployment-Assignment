0f6011406a9c534865562d829379f00abbf53fcb
Mitul Desai <34889290+Mitul2991@users.noreply.github.com>
2023-03-21 03:45:11 +0530

    Update download.py

--- download.py
+++ download.py
@@ -1,13 +1,17 @@
-# In this file, we define download_model
-# It runs during container build time to get model weights built into the container
-
-# In this example: A Huggingface BERT model
-
-from transformers import pipeline
+import torch
+import requests
+import io
 
 def download_model():
-    # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    # Define the URL of the Dropbox shared link
+    url = "https://www.dropbox.com/s/b7641ryzmkceoc9/pytorch_model_weights.pth?raw=1"
+
+    # Send a GET request to the URL to download the file
+    response = requests.get(url)
+
+    # Load the file contents into a PyTorch model
+    torch.load(io.BytesIO(response.content))
+
 
 if __name__ == "__main__":
     download_model()
