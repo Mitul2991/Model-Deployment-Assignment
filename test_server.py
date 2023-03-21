8b87d7a13079d41df6a54f2fb6d4b9f358455d45
Mitul Desai <34889290+Mitul2991@users.noreply.github.com>
2023-03-21 18:04:04 +0530

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -3,10 +3,15 @@ import base64
 from io import BytesIO
 from PIL import Image
 
-img = Image.open("n01440764_tench.jpeg")
+# img = Image.open("n01440764_tench.jpeg")
+img = Image.open("n01667114_mud_turtle.jpeg")
+width, height = img.size
 img_bytes = img.tobytes()
+img_data = base64.b64encode(img_bytes).decode('utf-8')
 model_inputs={
-    'prompt' : img_bytes
+    'prompt' : img_data,
+    'width' : width,
+    'height' : height
 }
 
 api_key = '1d77c452-d738-4da7-92b4-a839beb350de'
@@ -15,4 +20,4 @@ model_key = '5a8c7dad-db72-46e0-9887-36c570a2fbb4'
 # Testing the model
 out = banana.run(api_key, model_key, model_inputs)
 
-print (out)
+print ('OUTPUT :',out)
